507af059436dc36343b874d56f625052fd00c3fd
MartinDap <jorge.del.aguila@upeu.edu.pe>
2022-11-23 21:08:31 -0500

    Subiendo proyecto FreeDom

---
+++ src/app/pages/actividades/programas/form-modal/form-modal.component.ts
@@ -0,0 +1,55 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ProgramaService} from "../../../../providers/services/programa.service";
+
+@Component({
+  selector: 'app-form-modal',
+  templateUrl: './form-modal.component.html',
+  styleUrls: ['./form-modal.component.css']
+})
+export class FormModalComponent implements OnInit {
+
+  @Input() title: any;
+  @Input() progId: any;
+  @Input() item: any;
+  frmPrograma: FormGroup;
+  constructor(public activeModal: NgbActiveModal,
+              private formBuilder: FormBuilder,
+              private programaService: ProgramaService) { }
+
+  ngOnInit(): void {
+    this.formInit();
+    if (this.item) {
+      this.updateData();
+    }
+  }
+
+  formInit(): void {
+    const controls = {
+      progNombre: ['', [Validators.required]],
+      progDescripcion: ['', [Validators.required]],
+    };
+    this.frmPrograma = this.formBuilder.group(controls);
+  }
+
+  save(): void {
+    this.programaService.add$(this.frmPrograma.value).subscribe(response => {
+      if (response.success) {
+        this.activeModal.close({success: true, message:response.message});
+      }
+    });
+  }
+
+  update(): void {
+    this.programaService.update$(this.progId, this.frmPrograma.value).subscribe(response => {
+      if (response.success) {
+        this.activeModal.close({success: true, message:response.message});
+      }
+    });
+  }
+
+  updateData(): void {
+    this.frmPrograma.patchValue(this.item);
+  }
+}
