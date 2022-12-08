d5ad0cd1036143497a899825671f45ce4ce99311
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2022-12-08 14:26:02 -0500

    Actualizacion Material

--- src/app/pages/actividades/materiales/form-modal-materiales/form-modal-materiales.component.ts
+++ src/app/pages/actividades/materiales/form-modal-materiales/form-modal-materiales.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {MaterialesService} from "../../../../providers/services/materiales.service";
+import {TallerService} from "../../../../providers/services/taller.service";
 
 @Component({
   selector: 'app-form-modal-materiales',
@@ -13,43 +14,69 @@ export class FormModalMaterialesComponent implements OnInit {
   @Input() title: any;
   @Input() mateId: any;
   @Input() item: any;
+  talleres: any = [];
   frmMaterial: FormGroup;
   constructor(public activeModal: NgbActiveModal,
               private formBuilder: FormBuilder,
-              private materialesService: MaterialesService) { }
+              private materialesService: MaterialesService,
+              private tallerService: TallerService){}
 
   ngOnInit(): void {
+    this.getTaller();
     this.formInit();
-    if (this.item) {
+    if(this.item){
       this.updateData();
     }
   }
 
+  getTaller(): void {
+    this.tallerService.getAll$().subscribe(response => {
+      this.talleres = response.data || [];
+      console.log(this.talleres);
+    });
+  }
   formInit(): void {
     const controls = {
-      mateDescri: ['', [Validators.required]],
       mateName: ['', [Validators.required]],
+      mateDescri: ['', [Validators.required]],
+      tallId: ['', [Validators.required]],
     };
     this.frmMaterial = this.formBuilder.group(controls);
   }
 
   save(): void {
-    this.materialesService.add$(this.frmMaterial.value).subscribe(response => {
-      if (response.success) {
-        this.activeModal.close({success: true, message:response.message});
+    let data = Object.assign(this.frmMaterial.value,
+      {taller: {tallId: this.frmMaterial.value.tallId}});
+    this.materialesService.add$(data).subscribe(response => {
+      if (response.success){
+        this.activeModal.close({
+          success:true,
+          message: response.message
+        });
       }
     });
   }
 
   update(): void {
-    this.materialesService.update$(this.mateId, this.frmMaterial.value).subscribe(response => {
-      if (response.success) {
-        this.activeModal.close({success: true, message:response.message});
+    let data = Object.assign(this.frmMaterial.value,
+      {taller: {tallId: this.frmMaterial.value.tallId}});
+    console.log(data);
+    this.tallerService.update$(this.mateId, data).subscribe(response => {
+      if (response.success){
+        this.activeModal.close({
+          success:true,
+          message: response.message});
       }
     });
   }
 
+
   updateData(): void {
-    this.frmMaterial.patchValue(this.item);
+    let data = Object.assign(this.item,
+      {tallId: this.item.taller.tallId});
+    console.log("",data)
+    this.frmMaterial.patchValue(data);
   }
+
 }
+
