43b45ae8338abd01ee6f2a06edd4712ec3637860
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2022-12-14 09:16:59 -0500

    Asistrncia echa

--- src/app/pages/actividades/asistencias/asistencias.component.ts
+++ src/app/pages/actividades/asistencias/asistencias.component.ts
@@ -12,6 +12,7 @@ import {AsistenciaService} from "../../../providers/services/asistencia.service"
 export class AsistenciasComponent implements OnInit {
 
   asistencias: any = [];
+
   constructor(private asistenciaService: AsistenciaService,
               private modalService: NgbModal) { }
 
@@ -22,6 +23,7 @@ export class AsistenciasComponent implements OnInit {
   getAsistencias(): void {
     this.asistenciaService.getAll$().subscribe(response => {
       this.asistencias = response.data || [];
+      console.log(this.asistencias);
     });
   }
 
@@ -44,7 +46,7 @@ export class AsistenciasComponent implements OnInit {
         })
         this.getAsistencias();
       }
-    });
+    }).catch(err => {});
   }
 
   openModalEdit(item: any): any {
@@ -68,11 +70,12 @@ export class AsistenciasComponent implements OnInit {
         });
         this.getAsistencias();
       }
+    }).catch(res => {
     });
   }
   public onDelete(item: any): void {
     const ID = item.asisId;
-    const mensaje = '¿ Desea eliminar? : ' + ' ' + item.asisId;
+    const mensaje = '¿ Desea eliminar? : ' + ' ' + item.tipePersona + ' ?' ;;
     if (ID) {
       Swal.fire({
         title: 'Se eliminará el registro',
--- src/app/pages/actividades/asistencias/form-modal-asistencias/form-modal-asistencias.component.ts
+++ src/app/pages/actividades/asistencias/form-modal-asistencias/form-modal-asistencias.component.ts
@@ -2,8 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {TallerService} from "../../../../providers/services/taller.service";
-import {TipopersonaService} from "../../../../providers/services/tipopersona.service";
 import {AsistenciaService} from "../../../../providers/services/asistencia.service";
+import {PersonaService} from "../../../../providers/services/persona.service";
 
 @Component({
   selector: 'app-form-modal-asistencias',
@@ -15,25 +15,30 @@ export class FormModalAsistenciasComponent implements OnInit {
   @Input() title: any;
   @Input() asisId: any;
   @Input() item: any;
-  tipoPersonas: any = [];
+  personas: any = [];
   talleres: any = [];
 
   frmAsistencia: FormGroup;
   constructor(public activeModal: NgbActiveModal,
               private formBuilder: FormBuilder,
               private asistenciaService: AsistenciaService,
-              private tallerService: TallerService,
-              private tipopersonaService: TipopersonaService) {}
-
-  Id = document.getElementById('id');
+              private personaService: PersonaService,
+              private tallerService: TallerService){}
 
   ngOnInit(): void {
-    this.getTipoPersonas();
+    this.gePersonas();
     this.getTaller();
     this.formInit();
     if(this.item){
       this.updateData();
     }
+  }
+
+  gePersonas(): void {
+    this.personaService.getAll$().subscribe(response => {
+      this.personas = response.data || [];
+      console.log(this.personaService);
+    });
   }
 
   getTaller(): void {
@@ -43,29 +48,19 @@ export class FormModalAsistenciasComponent implements OnInit {
     });
   }
 
-  getTipoPersonas(): void {
-    this.tipopersonaService.getAll$().subscribe(response => {
-      this.tipoPersonas = response.data || [];
-      console.log(this.tipoPersonas);
-    });
-  }
-
   formInit(): void {
     const controls = {
       asisEsta: ['', [Validators.required]],
-      persNombre: ['', [Validators.required]],
-      persApPaterno: ['', [Validators.required]],
-      persApMaterno: ['', [Validators.required]],
+      persId: ['', [Validators.required]],
       tallId: ['', [Validators.required]],
-      tipeId: ['', [Validators.required]]
     };
     this.frmAsistencia = this.formBuilder.group(controls);
   }
 
   save(): void {
     let data = Object.assign(this.frmAsistencia.value,
-      {taller: {tallId: this.frmAsistencia.value.tallId}},
-      {tipoPersona: {tipeId: this.frmAsistencia.value.tipeId}});
+      {persona: {persId: this.frmAsistencia.value.persId}},
+      {taller: {tallId: this.frmAsistencia.value.tallId}});
     this.asistenciaService.add$(data).subscribe(response => {
       if (response.success){
         this.activeModal.close({
@@ -78,8 +73,8 @@ export class FormModalAsistenciasComponent implements OnInit {
 
   update(): void {
     let data = Object.assign(this.frmAsistencia.value,
-      {taller: {tallId: this.frmAsistencia.value.tallId}},
-      {tipoPersona: {tipeId: this.frmAsistencia.value.tipeId}});
+      {persona: {persId: this.frmAsistencia.value.persId}},
+      {taller: {tallId: this.frmAsistencia.value.tallId}});
     console.log(data);
     this.asistenciaService.update$(this.asisId, data).subscribe(response => {
       if (response.success){
@@ -92,8 +87,8 @@ export class FormModalAsistenciasComponent implements OnInit {
 
   updateData(): void {
     let data = Object.assign(this.item,
-      {tallId: this.item.taller.tallId},
-      {tipeId: this.item.tipoPersona.tipeId});
+      {persId: this.item.persona.persId},
+      {tallId: this.item.taller.tallId});
     console.log("AAAAAAA ",data)
     this.frmAsistencia.patchValue(data);
   }
