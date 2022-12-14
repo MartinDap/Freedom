48d9e6f23eab01b29949be26de5ec3732ac576a1
ZambranoGamer2002 <zambranogamerplay@gmail.com>
2022-12-14 14:27:00 -0500

    Asistencia actualizarcion

--- src/app/pages/actividades/talleres/form-modal-talleres/form-modal-talleres.component.ts
+++ src/app/pages/actividades/talleres/form-modal-talleres/form-modal-talleres.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {TallerService} from "../../../../providers/services/taller.service";
+import {ProgramaService} from "../../../../providers/services/programa.service";
 
 @Component({
   selector: 'app-form-modal-talleres',
@@ -14,13 +15,16 @@ export class FormModalTalleresComponent implements OnInit {
   @Input() tallId: any;
   @Input() item: any;
   talleres: any = [];
+  programas: any = [];
   frmTaller: FormGroup;
   constructor(public activeModal: NgbActiveModal,
               private formBuilder: FormBuilder,
+              private programaService: ProgramaService,
               private tallerService: TallerService) {}
 
   ngOnInit(): void {
     this.getTalleres();
+    this.getProgramas();
     this.formInit();
     if(this.item){
       this.updateData();
@@ -33,6 +37,12 @@ export class FormModalTalleresComponent implements OnInit {
       console.log(this.talleres);
     });
   }
+  getProgramas(): void {
+    this.programaService.getAll$().subscribe(response => {
+      this.programas = response.data || [];
+      console.log(this.programas);
+    });
+  }
 
   formInit(): void {
     const controls = {
@@ -42,29 +52,45 @@ export class FormModalTalleresComponent implements OnInit {
       tallHora: ['', [Validators.required]],
       tallLugar: ['', [Validators.required]],
       tallDesc: ['', [Validators.required]],
+      progId: ['', [Validators.required]],
       tallDireccion: ['', [Validators.required]],
     };
     this.frmTaller = this.formBuilder.group(controls);
   }
 
   save(): void {
-    this.tallerService.add$(this.frmTaller.value).subscribe(response => {
+    let data = Object.assign(this.frmTaller.value,
+      {programa: {progId: this.frmTaller.value.progId}});
+
+    this.tallerService.add$(data).subscribe(response => {
       if (response.success){
-        this.activeModal.close({success:true, message: response.message});
+        this.activeModal.close({
+          success:true,
+          message: response.message
+        });
       }
     });
   }
 
   update(): void {
-    this.tallerService.update$(this.tallId, this.frmTaller.value).subscribe(response => {
+    let data = Object.assign(this.frmTaller.value,
+      {programa: {progId: this.frmTaller.value.progId}});
+    console.log(data);
+    this.tallerService.update$(this.tallId, data).subscribe(response => {
       if (response.success){
-        this.activeModal.close({success:true, message: response.message});
+        this.activeModal.close({
+          success:true,
+          message: response.message});
       }
     });
   }
 
   updateData(): void {
-    this.frmTaller.patchValue(this.item);
+    let data = Object.assign(this.item,
+      {progId: this.item.programa.progId});
+    console.log("AAAAAAA ",data)
+    this.frmTaller.patchValue(data);
   }
 
 }
+
